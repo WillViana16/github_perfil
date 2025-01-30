05dae5975d13b4cc6971c7f56ac4fd102530064f
Willian <willianvia1@gmail.com>
2025-01-30 17:01:05 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//1- importação
+//2 - código do componente
+
+import Perfil from "./components/Perfil";
+import Formulario from './components/Formulario';
+
+
+function App(){
+  
+  return(
+    <>
+    <Perfil nome="Willian" endereco="https://github.com/WillViana16.png"/>
+    <Formulario />
+    
+    </>
+  )
+}
+
+  
+
+
+export default App
